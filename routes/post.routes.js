8c2ba55b9bb791dd2cb374088bb11e20e5f9b9a2
tegra10 <tegkit30@gmail.com>
2024-07-01 00:00:52 +0100

    authentification reussie

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -8,20 +8,16 @@ const {
     deletePosts
 } = require("../controllers/post.controller.js");
 
-const {login,getOneUser,logout}=require('../auth/user.auth.js')
+
 
 // kes authentifications d'enregistrement sur le site
-Router.post("/register", setPosts);
+Router.post("/", setPosts);
 
 // les operations sur les données déjà enregistrer
 Router.get("/", getPosts);
 Router.put("/edit-post/:id", editPosts);
 Router.delete("/delete-post/:id", deletePosts);
 
-// requettes d'authentifications aprés enregistrement
 
-Router.post("/login", login);
-Router.get("/:id", getOneUser);
-Router.get("/logout", logout);
 
 module.exports = Router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,13 +7,18 @@ const {
     deleteUsers
 } = require("../controllers/user.controller.js");
 
-
+const { login, getOneUser, logout } = require("../auth/user.auth.js");
 
 // requettes liées à l'utilisateur
-Router.post("/", setUsers);
+Router.post("/register", setUsers);
 Router.get("/", getUsers);
 Router.put("/edit-user/:id", editUsers);
 Router.delete("/delete-user/:id", deleteUsers);
 
+// requettes d'authentifications aprés enregistrement
+
+Router.post("/login", login);
+Router.get("/:id", getOneUser);
+Router.get("/logout", logout);
 
 module.exports = Router;
