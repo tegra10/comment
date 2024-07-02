1871275b18c9ae9bb7d69ec6d5011c82508cccdb
tegra10 <tegkit30@gmail.com>
2024-07-03 00:03:46 +0100

    requireAuth finish

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 "use strict";
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const Sequelize = require("sequelize");
 const app = express();
 const sequelize = require("./config/config.js");
-const { checkUser } = require("./middleware/auth.middleware.js");
+const { checkUser, requireAuth } = require("./middleware/auth.middleware.js");
 const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -13,6 +14,9 @@ app.use(cookieParser());
 app.use("/user/api", require("./routes/user.routes.js"));
 app.use("/post", require("./routes/post.routes.js"));
 app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+    res.status(200).json(res.locals.user.id);
+});
 app.listen(port, () => {
     console.log(`the server run on port ${port}`);
 });
--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,28 +1,38 @@
-"use stric";
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.model.js");
 
 module.exports.checkUser = (req, res, next) => {
-    const token = res.cookies.jwt;
-    try {
-        if (!token) {
-            jwt.verify(token, process.env.TOKEN, async (err, decodedToken) => {
-                if (err) {
-                    res.locals.user = null;
-                    res.cookie("token", "", { maxAge: 1 });
-                    next();
-                } else {
-                    let user = await userModel.findByPk(decodedToken.id);
-                    res.locals.user = user;
-                    console.log(res.locals.user);
-                    next();
-                }
-            });
-        } else {
-            res.locals.user = null;
-            next();
-        }
-    } catch (err) {
-        console.error(err);
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, process.env.TOKEN, async (err, decodedToken) => {
+            if (err) {
+                res.locals.user = null;
+                res.cookie("jwt", "", { maxAge: 1 });
+                next();
+            } else {
+                let user = await userModel.findByPk(decodedToken.id);
+                res.locals.user = user;
+                console.log(res.locals.user);
+                next();
+            }
+        });
+    } else {
+        res.locals.user = null;
+        next();
+    }
+};
+
+module.exports.requireAuth = (req, res) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, process.env.TOKEN, async (err, decodedToken) => {
+            if (err) {
+              console.log(err)
+            }else{
+              console.log(decodedToken.id)
+            }
+        });
+    }else{
+      console.log('no TOKEN')
     }
 };
